b1218496d68657af97dda6daf9a59cea8df9e8b4
Harsh Yadav <pharshyaduvanshi@gmail.com>
2023-09-24 17:28:33 +0530

    First Commit

---
+++ Pattern4.java
@@ -0,0 +1,10 @@
+public class Pattern4 {
+    public static void main(String[] args) {
+        for(int row=1;row<=5;row++){
+            for(int j=1;j<=5;j++){
+                System.out.print("*");
+            }System.out.println();
+        }
+    }
+    
+}
